29c78d663760fecf63478bb352572cf5c72587b9
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-09 20:50:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ let index = 0
 function init() {
   // Write your JavaScript code inside the init() function
   document.addEventListener('keydown', function(e) {
-    const key = e.which;
+    const key = parseInt(e.detail || e.keyCode);
 
     if (key === code[index]) {
       index++
